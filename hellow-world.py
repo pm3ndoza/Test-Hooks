b1b2f4c0fe5002791bca227d50d9fe747a93e646
Pedro Mendoza <pmendoza2853@gmail.com>
2022-09-27 19:29:45 -0300

    test

---
+++ hellow-world.py
@@ -0,0 +1,11 @@
+print("Hola")
+freecodecamp = "freeCodeCamp"
+print (freecodecamp[:8])
+print (freecodecamp[4:])
+print (freecodecamp[:8:2])
+
+
+print (freecodecamp[4::3])
+print (freecodecamp[::-2])
+
+print (freecodecamp[::-1])
